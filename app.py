80586245acc63edc515173a83e522b2d8ae652b0
Bobur <ubobur2197@gmail.com>
2026-04-10 15:38:23 +0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# 1
+roy = ["10", "20", "30"]
+print([int(x) + 10 for x in roy])
+
+# 2
+roy = [9, 16, 25]
+print([x ** 0.5 for x in roy])
+
+# 3
+roy = ["olma", "anor", "uzum"]
+print([x[0] for x in roy])
+
+# 4
+roy = [11, 22, 33, 44]
+print([str(x) for x in roy])
+
+# 5
+roy = ["python", "map", "function"]
+print([len(x) * 2 for x in roy])
